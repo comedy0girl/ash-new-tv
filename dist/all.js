c9cfc52ef1b5065797af3deeb2f609d4c2882200
Ashley Miller <comedy.writer.ash@gmail.com>
2018-10-27 13:56:59 +0100

    changed from dev menu to live

--- dist/all.js
+++ dist/all.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 var windWidth = $(window).width();
     if(windWidth < 802) {
-        $('ul#menu-header-menu').appendTo($('#the-mobi'));
-        // $('ul#menu-new-header').appendTo($('#the-mobi'));
+        //$('ul#menu-header-menu').appendTo($('#the-mobi'));
+        $('ul#menu-new-header').appendTo($('#the-mobi'));
     };
 
     $('.good-burger').click(function() {
